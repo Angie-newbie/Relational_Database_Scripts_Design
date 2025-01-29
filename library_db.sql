acf886f197fe1a9ed6bd8bda18403adba62d6690
Angie <angel890817@hotmail.com>
2025-01-29 10:45:24 +1000

    Created tables

--- library_db.sql
+++ library_db.sql
@@ -1,3 +1,39 @@
+drop table if exists authors;
+drop table if exists members;
+drop table if exists categories;
+drop table if exists books;
+drop table if exists loans;
+
+
 create table authors (
-    id serial primary key
-)
+    id serial primary key,
+    first_name varchar(50),
+    last_name varchar(50)
+);
+
+create table members (
+    id serial primary key,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(150) not null,
+    dob date,
+    phone varchar(100) not null
+);
+
+create table categories(
+    id serial primary key,
+    name varchar(50) not null
+);
+
+create table books(
+    id serial primary key,
+    title varchar (100) not null,
+    publisher varchar(100),
+    publish_date date
+);
+
+create table loans(
+    id serial primary key,
+    loan_date date not null,
+    due_date date not null
+);
